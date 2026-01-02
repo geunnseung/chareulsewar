45f9680665f63f546dd188b7da7cefa9aa4f1cf1
geunnseung <bsg1017@gmail.com>
2026-01-02 22:09:54 +0900

    chore: initial commit

---
+++ src/main/java/com/geunnseung/chareulsewar/ChareulsewarApplication.java
@@ -0,0 +1,13 @@
+package com.geunnseung.chareulsewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChareulsewarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChareulsewarApplication.class, args);
+    }
+
+}
